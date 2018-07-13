806647e3e9b3108e678f0dd364dbc2635caa1986
Jenn Briden <jennbriden@gmail.com>
2018-07-12 17:19:57 -0700

    Adding initial files for demo project

---
+++ src/main/java/com/jbriden/jbridendemomavenapp/JbridenDemoMavenAppApplication.java
@@ -0,0 +1,12 @@
+package com.jbriden.jbridendemomavenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbridenDemoMavenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbridenDemoMavenAppApplication.class, args);
+	}
+}
